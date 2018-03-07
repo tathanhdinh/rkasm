ed54d21d6db477433b2c99b7d0c9dd46a8587b01
TA Thanh Dinh <tathanhdinh@gmail.com>
2018-03-07 18:05:27 +0100

    employe take_values to specify list of possible values

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ static ARGUMENT_BASE: &'static str = "base address";
 static ARGUMENT_MODE: &'static str = "assembling mode";
 
 fn main() {
-    // println!("Hello, world!");
     let matches = clap::App::new(APPLICATION_NAME)
         .version(APPLICATION_VERSION)
         .author(APPLICATION_AUTHOR)
@@ -32,7 +31,8 @@ fn main() {
              .short("m")
              .long("mode")
              .takes_value(true)
-             .default_value("x64"))
+             .default_value("x64")
+             .possible_values(&["x64", "x32"]))
         .get_matches();
 
     let asm_mode = if matches.is_present(ARGUMENT_MODE) {
@@ -44,9 +44,7 @@ fn main() {
                 keystone::MODE_64
             },
             _ => {
-                // keystone::MODE_64
-                println!("{}", "bad assembling mode (should be either x32 or x64)");
-                return;
+                unreachable!();
             }
         }
     }
@@ -71,57 +69,45 @@ fn main() {
     let asm_code: Vec<_> = asm_code.split(';').collect();
     let asm_code: Vec<_> = asm_code.into_iter().map(|ins| ins.trim()).collect();
 
-    let mut asm_results: Vec<_> = Vec::new();
-    let mut asm_result;
+    let mut assembled_strings: Vec<_> = Vec::new();
+    let mut assembled_ins_string;
     let mut ins_base_address = base_address;
     for ins in asm_code {
         let assembling_result = 
-            if let Ok(result) = engine.asm(ins.to_string(), ins_base_address) {
-                let opcode_len = result.bytes.len();
-                let opcode_strs: Vec<_> = result.bytes
+            if let Ok(assembled_ins) = engine.asm(ins.to_string(), ins_base_address) {
+                let opcode_len = assembled_ins.bytes.len();
+                let opcode_strs: Vec<_> = assembled_ins.bytes
                     .into_iter()
                     .map(|opc| format!("{:02x}", opc))
                     .collect();
                 let opcode_string = opcode_strs.join(" ");
                 
-                asm_result = format!("0x{:x}\t{}\t{}", ins_base_address, &opcode_string, ins);
+                assembled_ins_string = format!("0x{:x}\t{}\t{}", ins_base_address, &opcode_string, ins);
                 // asm_results.push(asm_result);
                 // Ok(format!("0x{:016x}\t{}\t{}", ins_base_address, &opcode_string, ins))
                 ins_base_address += opcode_len as u64;
-                Ok(())
+                Some(())
             }
             else {
-                asm_result = format!("0x{:x}\t{}\t{}", ins_base_address, "error", ins);
+                assembled_ins_string = format!("0x{:x}\t{}\t{}", ins_base_address, "error", ins);
                 // asm_results.push(asm_result);
                 // break;
                 // Err(format!("0x{:016x}\t{}\t{}", ins_base_address, "error", ins))
-                Err(())
+                // Err(())
+                None
             };
 
-        asm_results.push(asm_result);
-        if assembling_result.is_err() {
+        assembled_strings.push(assembled_ins_string);
+        if assembling_result.is_none() {
             break;
         }
     }
-    let asm_results = asm_results.join("\r\n");
+    let asm_results = assembled_strings.join("\r\n");
     // println!("{}", asm_results.len());
 
     // let mut tw = tabwriter::TabWriter::new(Vec::new()).padding(2);
     let mut tw = tabwriter::TabWriter::new(std::io::stdout()).padding(4);
-    // tw.w
     // write!(&mut tw, &asm_results);
     writeln!(&mut tw, "{}", asm_results).unwrap();
     tw.flush().unwrap();
-
-    // if let Ok(asm_result) = engine.asm(asm_code.to_string(), base_address) {
-    //     // println!("{}", asm_result);
-    //     for byte in asm_result.bytes {
-    //         print!("{:02x} ", byte);
-    //     }
-    //     println!("");
-    // }
-    // else {
-    //     println!("{}", "could not assemble")
-    // }
-
 }
