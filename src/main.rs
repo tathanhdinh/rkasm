89164759202f259ed56d4144b43e1e82bcf41bd0
TA Thanh Dinh <tathanhdinh@gmail.com>
2018-04-04 18:20:23 +0200

    works!!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#![feature(fs_read_write)]
+
 extern crate keystone;
 extern crate tabwriter;
 #[macro_use] extern crate clap;
 #[macro_use] extern crate failure;
 
-use std::io::{Read, Write, BufRead};
+use std::io::{Write, BufRead};
 
 static APPLICATION_NAME: &'static str = "rkasm";
 static APPLICATION_VERSION: &'static str = "0.1.0";
@@ -14,102 +16,12 @@ static ARGUMENT_ASM: &'static str = "x86 assembly";
 static ARGUMENT_BASE: &'static str = "base address";
 static ARGUMENT_MODE: &'static str = "assembling mode";
 static ARGUMENT_FILE: &'static str = "input x86 assembly file";
+static ARGUMENT_OUTPUT_FILE: &'static str = "output binary file";
 
 fn main() {
-    let matches = clap::App::new(APPLICATION_NAME)
-        .version(APPLICATION_VERSION)
-        .author(APPLICATION_AUTHOR)
-        .about(APPLICATION_ABOUT)
-        .arg(clap::Arg::with_name(ARGUMENT_ASM)
-             .required(true)
-             .index(1))
-        .arg(clap::Arg::with_name(ARGUMENT_BASE)
-             .short("b")
-             .long("base")
-             .takes_value(true)
-             .default_value("0"))
-        .arg(clap::Arg::with_name(ARGUMENT_MODE)
-             .short("m")
-             .long("mode")
-             .takes_value(true)
-             .default_value("x64")
-             .possible_values(&["x64", "x32"]))
-        .get_matches();
-
-    let asm_mode = if matches.is_present(ARGUMENT_MODE) {
-        match matches.value_of(ARGUMENT_MODE).unwrap() {
-            "x32" => {
-                keystone::gen::KS_MODE_32
-            },
-            "x64" => {
-                keystone::gen::KS_MODE_64
-            },
-            _ => {
-                unreachable!();
-            }
-        }
+    if let Err(err) = run() {
+        println!("{}", err);
     }
-    else {
-        keystone::gen::KS_MODE_64
-    };
-
-    let base_address = if matches.is_present(ARGUMENT_BASE) {
-        value_t!(matches, ARGUMENT_BASE, u64).unwrap_or(0x0)
-    }
-    else {
-        0x0
-    };
-
-    let engine = keystone::Keystone::new(keystone::gen::KS_ARCH_X86, asm_mode)
-        .expect("could not initialize Keystone engine");
-    engine.option(keystone::gen::KS_OPT_SYNTAX, keystone::gen::KS_OPT_SYNTAX_NASM)
-        .expect("could not set option to NASM syntax");
-
-    let asm_code = matches.value_of(ARGUMENT_ASM).unwrap(); // should not panic since required
-    let asm_code: Vec<_> = asm_code.split(';').collect();
-    let asm_code: Vec<_> = asm_code.into_iter().map(|ins| ins.trim()).collect();
-
-    let mut assembled_strings: Vec<_> = Vec::new();
-    let mut assembled_ins_string;
-    let mut ins_base_address = base_address;
-    for ins in asm_code {
-        let assembling_result = 
-            if let Ok(assembled_ins) = engine.asm(&ins, ins_base_address) {
-                let opcode_len = assembled_ins.encoding.len();
-                let opcode_strs: Vec<_> = assembled_ins.encoding
-                    .into_iter()
-                    .map(|opc| format!("{:02x}", opc))
-                    .collect();
-                let opcode_string = opcode_strs.join(" ");
-                
-                assembled_ins_string = format!("0x{:x}\t{}\t{}", ins_base_address, &opcode_string, ins);
-                // asm_results.push(asm_result);
-                // Ok(format!("0x{:016x}\t{}\t{}", ins_base_address, &opcode_string, ins))
-                ins_base_address += opcode_len as u64;
-                Some(())
-            }
-            else {
-                assembled_ins_string = format!("0x{:x}\t{}\t{}", ins_base_address, "error", ins);
-                // asm_results.push(asm_result);
-                // break;
-                // Err(format!("0x{:016x}\t{}\t{}", ins_base_address, "error", ins))
-                // Err(())
-                None
-            };
-
-        assembled_strings.push(assembled_ins_string);
-        if assembling_result.is_none() {
-            break;
-        }
-    }
-    let asm_results = assembled_strings.join("\r\n");
-    // println!("{}", asm_results.len());
-
-    // let mut tw = tabwriter::TabWriter::new(Vec::new()).padding(2);
-    let mut tw = tabwriter::TabWriter::new(std::io::stdout()).padding(4);
-    // write!(&mut tw, &asm_results);
-    writeln!(&mut tw, "{}", asm_results).unwrap();
-    tw.flush().unwrap();
 }
 
 fn run() -> Result<(), failure::Error> {
@@ -125,6 +37,10 @@ fn run() -> Result<(), failure::Error> {
              .long("file")
              .takes_value(true)
              .conflicts_with(ARGUMENT_ASM))
+        .arg(clap::Arg::with_name(ARGUMENT_OUTPUT_FILE)
+             .short("o")
+             .long("out")
+             .takes_value(true))
         .arg(clap::Arg::with_name(ARGUMENT_BASE)
              .short("b")
              .long("base")
@@ -167,9 +83,25 @@ fn run() -> Result<(), failure::Error> {
     engine.option(keystone::gen::KS_OPT_SYNTAX, keystone::gen::KS_OPT_SYNTAX_NASM)
         .expect("could not set option to NASM syntax");
 
-    // let mut asm_code: Vec<&str> = vec![];
+    // let mut output_file = std::io::BufWriter::new(Box::new(std::io::stdout()) as Box<std::io::Write>);
+    // let mut output_to_file = false;
+    // if matches.is_present(ARGUMENT_OUTPUT_FILE) {
+    //     output_to_file = true;
+    //     // output_file = matches.value_of(ARGUMENT_OUTPUT_FILE).unwrap();
+    //     let file = std::fs::File::create(matches.value_of(ARGUMENT_OUTPUT_FILE).unwrap())?;
+    //     output_file = std::io::BufWriter::new(Box::new(file) as Box<std::io::Write>)
+    // };
+
+    let (mut output_file, output_to_file) = 
+        if matches.is_present(ARGUMENT_OUTPUT_FILE) {
+            let file = std::fs::File::create(matches.value_of(ARGUMENT_OUTPUT_FILE).unwrap())?;
+            (std::io::BufWriter::new(Box::new(file) as Box<std::io::Write>), true)
+        }
+        else {
+            (std::io::BufWriter::new(Box::new(std::io::stdout()) as Box<std::io::Write>), false)
+        };
+
     let lines;
-    // let mut asm_code = vec![];
     let asm_code = 
         if matches.is_present(ARGUMENT_FILE) {
             let input_file = std::fs::File::open(matches.value_of(ARGUMENT_FILE).unwrap())?;
@@ -197,7 +129,7 @@ fn run() -> Result<(), failure::Error> {
             if let Ok(assembled_ins) = engine.asm(&ins, ins_base_address) {
                 let opcode_len = assembled_ins.encoding.len();
                 let opcode_strs: Vec<_> = assembled_ins.encoding
-                    .into_iter()
+                    .iter()
                     .map(|opc| format!("{:02x}", opc))
                     .collect();
                 let opcode_string = opcode_strs.join(" ");
@@ -206,6 +138,12 @@ fn run() -> Result<(), failure::Error> {
                 // asm_results.push(asm_result);
                 // Ok(format!("0x{:016x}\t{}\t{}", ins_base_address, &opcode_string, ins))
                 ins_base_address += opcode_len as u64;
+
+                if output_to_file {
+                    // std::fs::write(output_file, &assembled_ins.encoding)?;
+                    output_file.write_all(&assembled_ins.encoding)?;
+                }
+
                 Some(())
             }
             else {
